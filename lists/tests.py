0eb0b0cb9b4ce00d3180f839b5ad061e92837fb3
Marc Beitchman <mbeitchman@gmail.com>
2014-05-21 22:36:56 -0700

    Add unique lists

--- lists/tests.py
+++ lists/tests.py
@@ -4,9 +4,37 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
+
+class NewItemTest(TestCase):
+	
+	def test_can_save_a_POST_request_to_an_existing_list(self):
+		other_list = List.objects.create()
+		correct_list = List.objects.create()
+
+		self.client.post(
+			'/lists/%d/add_item' % (correct_list.id,),
+			data={'item_text': 'A new item for an existing list'}
+		)
+
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new item for an existing list')
+		self.assertEqual(new_item.list, correct_list)
+
+	def test_redirects_to_list_view(self):
+		other_list = List.objects.create()
+		correct_list = List.objects.create()
+
+		response = self.client.post(
+			'/lists/%d/add_item' % (correct_list.id,),
+			data={'item_text': 'A new item for an existing list'}
+		)
+
+		self.assertRedirects(response, 'lists/%d/' % (correct_list.id,))
 
 class NewListTest(TestCase):
+
 	def test_saving_a_POST_request(self):
 		self.client.post(
 			'/lists/new',
@@ -21,23 +49,41 @@ class NewListTest(TestCase):
 			'/lists/new',
 			data={'item_text': 'A new list item'})
 
+		new_list = List.objects.first()
 		self.assertEqual(response.status_code, 302)
-		self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+		self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
 
 
 class ListViewTest(TestCase):
+
 	def test_uses_list_template(self):
-		response = self.client.get('/lists/the-only-list-in-the-world/')
+		list_ = List.objects.create()
+		response = self.client.get('/lists/%d/' % (list_.id,))
 		self.assertTemplateUsed(response, 'list.html')
 
-	def test_displays_all_items(self):
-		Item.objects.create(text='foo foo')
-		Item.objects.create(text='bar bar')
+	def test_displays_only_items_for_that_list(self):
+		correct_list = List.objects.create()
+		Item.objects.create(text='foo foo', list=correct_list)
+		Item.objects.create(text='bar bar', list=correct_list)
 
-		response = self.client.get('/lists/the-only-list-in-the-world/')
+		other_list = List.objects.create()
+		Item.objects.create(text='other list item 1', list=other_list)
+		Item.objects.create(text='other list item 2', list=other_list)
+
+		response = self.client.get('/lists/%d/' % (correct_list.id,))
 
 		self.assertContains(response, 'foo foo')
 		self.assertContains(response, 'bar bar')
+		self.assertNotContains(response, 'other list item 1')
+		self.assertNotContains(response, 'other list item 2')
+
+	def test_passes_correct_view_to_template(self):
+		correct_list = List.objects.create()
+		other_list = List.objects.create()
+
+		response = self.client.get('/lists/%d/' % (correct_list.id,))
+
+		self.assertEqual(response.context['list'], correct_list)
 
 class HomePageTest(TestCase):
 
@@ -53,19 +99,27 @@ class HomePageTest(TestCase):
 
 		self.assertEqual(response.content.decode(), expected_html)
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
 	def test_saving_and_retrieving_items(self):
+		list_ = List()
+		list_.save()
+
 		first_item_text = 'The first (ever) list item'
 		second_item_text = 'The second item'
 
 		first_item = Item()
 		first_item.text = first_item_text
+		first_item.list = list_
 		first_item.save()
 
 		second_item = Item()
 		second_item.text = second_item_text
+		second_item.list = list_
 		second_item.save()
+
+		saved_list = List.objects.first()
+		self.assertEqual(saved_list, list_)
 
 		saved_items = Item.objects.all()
 		self.assertEqual(saved_items.count(), 2)
@@ -75,3 +129,5 @@ class ItemModelTest(TestCase):
 
 		self.assertEqual(first_item_text, first_saved_item.text)
 		self.assertEqual(second_item_text, second_saved_item.text)
+		self.assertEqual(first_saved_item.list, list_)
+		self.assertEqual(second_saved_item.list, list_)
