6746b8e853810a8de0db3e69224a240b7441bbc5
Marc Beitchman <mbeitchman@gmail.com>
2014-05-21 21:05:13 -0700

    Factor creation into seperate view functions

--- lists/tests.py
+++ lists/tests.py
@@ -6,6 +6,25 @@ from django.template.loader import render_to_string
 from lists.views import home_page
 from lists.models import Item
 
+class NewListTest(TestCase):
+	def test_saving_a_POST_request(self):
+		self.client.post(
+			'/lists/new',
+			data={'item_text': 'A new list item'})
+
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new list item')
+
+	def test_redirects_after_POST(self):
+		response = self.client.post(
+			'/lists/new',
+			data={'item_text': 'A new list item'})
+
+		self.assertEqual(response.status_code, 302)
+		self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+
+
 class ListViewTest(TestCase):
 	def test_uses_list_template(self):
 		response = self.client.get('/lists/the-only-list-in-the-world/')
@@ -33,34 +52,6 @@ class HomePageTest(TestCase):
 		response = home_page(request)
 
 		self.assertEqual(response.content.decode(), expected_html)
-
-	def test_home_page_only_saves_items_when_necessary(self):
-		request = HttpRequest()
-		home_page(request)
-		self.assertEqual(Item.objects.count(), 0)
-
-	def test_home_page_can_save_a_POST_request(self):
-		item_text = 'new list item'
-		request = HttpRequest()
-		request.method = 'POST'
-		request.POST['item_text'] = item_text
-
-		response = home_page(request)
-
-		self.assertEqual(Item.objects.count(), 1)
-		new_item = Item.objects.first()
-		self.assertEqual(new_item.text, item_text)
-
-	def test_home_page_redirects_after_POST(self):
-		item_text = 'new list item'
-		request = HttpRequest()
-		request.method = 'POST'
-		request.POST['item_text'] = item_text
-
-		response = home_page(request)
-
-		self.assertEqual(response.status_code, 302)
-		self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
 class ItemModelTest(TestCase):
 
